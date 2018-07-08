fd9534410630ba04ad8a2c10ed10b757487b1aeb
Prashant Rane <prrane@microsoft.com>
2018-07-08 15:49:22 +0530

    Added `Model` classes and Unit Tests to validate those.

---
+++ MovieDBClient/MovieDBClient/Models/Models.swift
@@ -0,0 +1,84 @@
+//
+//  Models.swift
+//  MovieDBClient
+//
+//  Created by Prashant Rane on 08/07/18.
+//  Copyright © 2018 Prashant Rane. All rights reserved.
+//
+
+import Foundation
+
+/**
+ `APIResponse->response` Sample data
+  "title": "Batman",
+  "release_date": "1989-06-23",
+  "overview": "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker",
+  "poster_path": "\/kBf3g9crrADGMc2AMAMlLBgSm2h.jpg"
+  */
+struct Movie: Codable {
+  let title: String
+  let releaseDate: String
+  let overview: String
+  let posterPath: String? // this is optional as per API Docs
+}
+
+/**
+ `APIResponse` Sample Data
+   "page": 1000,
+   "total_results": 106,
+   "total_pages": 6,
+   "results": []
+
+      OR
+
+   "status_code": 7,
+   "status_message": "Invalid API key: You must be granted a valid key.",
+   "success": false
+
+    OR
+
+  "errors": ["query must be provided"]
+ */
+struct APIResponse: Codable {
+  // Success
+  let currentPage: Int  // Starts from `1`
+  let totalResults: Int
+  let totalPages: Int
+  let movies: [Movie]
+
+  // Error
+  let statusMessage: String?
+  let success: Bool?
+  let errors: [String]?
+
+  private enum CodingKeys: String, CodingKey {
+    // Success
+    case currentPage = "page"
+    case totalResults
+    case totalPages
+    case movies = "results"
+
+    // Error
+    case statusMessage
+    case success
+    case errors
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    let currentPage = try? container.decode(Int.self, forKey: .currentPage)
+    self.currentPage = currentPage ?? 0
+    let totalResults = try? container.decode(Int.self, forKey: .totalResults)
+    self.totalResults = totalResults ?? 0
+    let totalPages = try? container.decode(Int.self, forKey: .totalPages)
+    self.totalPages = totalPages ?? 0
+    let movies = try? container.decode([Movie].self, forKey: .movies)
+    self.movies = movies ?? []
+
+    statusMessage = try? container.decode(String.self, forKey: .statusMessage)
+    success = try? container.decode(Bool.self, forKey: .success)
+    errors = try? container.decode([String].self, forKey: .errors)
+  }
+
+}
--- MovieDBClient/MovieDBClientTests/MovieDBClientTests.swift
+++ MovieDBClient/MovieDBClientTests/MovieDBClientTests.swift
@@ -9,28 +9,144 @@
 import XCTest
 @testable import MovieDBClient
 
+private enum TestDataType {
+  case success, successWithNoPoster, successWithNoResults, failure, error
+
+  var fileName: String {
+    switch self {
+    case .success:                return "valid_response"
+    case .successWithNoPoster:    return "valid_response_no_poster"
+    case .successWithNoResults:   return "valid_response_no_results"
+    case .failure:                return "failure_response"
+    case .error:                  return "error_response"
+    }
+  }
+}
+
 class MovieDBClientTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+  private func jsonData(for type: TestDataType) -> Data {
+    guard let path = Bundle(for: MovieDBClientTests.self).path(forResource: type.fileName, ofType: "json") else {
+      XCTFail("\nCould not get path for file : \(type.fileName).json \n")
+      return Data(bytes: [])
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    guard let data = FileManager.default.contents(atPath: path) else {
+      XCTFail("\nCould not get data from file path: \(path)\n")
+      return Data(bytes: [])
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    return data
+  }
+
+  private func valiateForSuccess(response: APIResponse) {
+    if let success = response.success, !success {
+      XCTFail("\nFound failure in valid response: => \(String(describing: response.statusMessage))\n")
+      return
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    guard response.errors == nil else {
+      XCTFail("\nFound error in valid response: => \(String(describing: response.errors))\n")
+      return
     }
-    
+  }
+
+  func testMovieSearchSuccess() {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    do {
+      let response = try decoder.decode(APIResponse.self, from: jsonData(for: .success))
+      valiateForSuccess(response: response)
+
+      XCTAssert(response.currentPage == 1, "Current page index decoded is wrong")
+
+      guard !response.movies.isEmpty else {
+        XCTFail("No Movies found in valid response\n")
+        return
+      }
+
+      let movie = response.movies.first!
+
+      XCTAssert(movie.title == "Batman", "Movie title does not matches with test data")
+      XCTAssert(movie.releaseDate == "1989-06-23", "Movie release date does not matches with test data")
+      XCTAssert(movie.posterPath == "/kBf3g9crrADGMc2AMAMlLBgSm2h.jpg", "Movie poster path is decoded incorrectly")
+    }
+    catch let error {
+      XCTFail("\n==>\n\(error)\n")
+    }
+  }
+
+  func testMovieSearchSuccessWithNoPoster() {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    do {
+      let response = try decoder.decode(APIResponse.self, from: jsonData(for: .successWithNoPoster))
+      valiateForSuccess(response: response)
+
+      guard !response.movies.isEmpty else {
+        XCTFail("No Movies found in valid response\n")
+        return
+      }
+
+      let movie = response.movies.first!
+
+      XCTAssert(movie.title == "Batman", "Movie title does not matches with test data")
+      XCTAssert(movie.releaseDate == "1989-06-23", "Movie release date does not matches with test data")
+      XCTAssert(movie.posterPath == nil, "Movie poster path is decoded incorrectly")
+    }
+    catch let error {
+      XCTFail("\n==>\n\(error)\n")
+    }
+  }
+
+  func testMovieSearchSuccessWithNoResult() {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    do {
+      let response = try decoder.decode(APIResponse.self, from: jsonData(for: .successWithNoResults))
+      valiateForSuccess(response: response)
+      XCTAssert(response.movies.isEmpty, "No Movies should be available in empty response\n")
+    }
+    catch let error {
+      XCTFail("\n==>\n\(error)\n")
+    }
+  }
+
+  func testMovieSearchFailure() {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    do {
+      let response = try decoder.decode(APIResponse.self, from: jsonData(for: .failure))
+
+      XCTAssert(response.currentPage == 0, "Current page index decoded is wrong")
+      XCTAssert(response.totalPages == 0, "Total pages decoded is wrong")
+      XCTAssert(response.totalResults == 0, "Total results decoded is wrong")
+      XCTAssert(!(response.success ?? true), "failure response should have success == false \n")
+
+      XCTAssert(response.movies.isEmpty, "No Movies should be available in case of failure response\n")
+    }
+    catch let error {
+      XCTFail("\n==>\n\(error)\n")
+    }
+  }
+
+  func testMovieSearchError() {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    do {
+      let response = try decoder.decode(APIResponse.self, from: jsonData(for: .error))
+
+      XCTAssert(!(response.errors?.isEmpty ?? true), "No Movies should be available in case of failure response\n")
+
+      XCTAssert(response.currentPage == 0, "Current page index decoded is wrong")
+      XCTAssert(response.totalPages == 0, "Total pages decoded is wrong")
+      XCTAssert(response.totalResults == 0, "Total results decoded is wrong")
+
+      XCTAssert(response.movies.isEmpty, "No Movies should be available in case of failure response\n")
+    }
+    catch let error {
+      XCTFail("\n==>\n\(error)\n")
+    }
+  }
+
 }
